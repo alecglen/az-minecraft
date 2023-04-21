9bfd6d88b0e167e7e5dc54a86e78350a1da0e99a
Alec Ostrander <alec.ostrander@growersedge.com>
2023-04-21 14:49:48 -0500

    test returning html

--- hello/__init__.py
+++ hello/__init__.py
@@ -18,7 +18,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     name = "Taylor"
 
     if name:
-        return func.HttpResponse(f"Hello, {name}!")
+        return func.HttpResponse(f'<h1 style="color:red;">Hello, {name}!</h1>')
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",

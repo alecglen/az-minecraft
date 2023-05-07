ef0374b7e6f47f0bc3d15cc624de4e55bd6a35ed
Alec Ostrander <alec.ostrander@growersedge.com>
2023-05-07 11:04:20 -0500

    add error boundary

--- hello/logic.py
+++ hello/logic.py
@@ -1,6 +1,8 @@
 from wrappers import http
 from errors import ValidationError
 
+from azure.functions import HttpRequest
+
 
 @http(template="hello/template.html")
 def main(**inputs: dict):
@@ -13,3 +15,21 @@ def validate(inputs: dict):
         return inputs.get("name")
     
     raise ValidationError("Request worked, but you didn't give me a name!")
+
+
+# -----------------------------------------------------------------------------
+# For local calls
+
+if __name__ == "__main__":
+    # Run a test and output the result
+    test_request = HttpRequest(
+        'POST',
+        'not-used.url',
+        params={"name": "Test"},
+        body=bytes()
+    )
+    response = main(test_request)
+    print(
+        response.status_code, ":",
+        response.get_body().decode("utf-8")
+    )

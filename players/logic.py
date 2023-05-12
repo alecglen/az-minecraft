0f89b6472dbb85830bf56d20e6a889aec15f9b4e
Alec Ostrander <alec.ostrander@growersedge.com>
2023-05-12 09:36:03 -0500

    add player by id logic

--- players/logic.py
+++ players/logic.py
@@ -7,18 +7,30 @@ from wrappers import http
 from errors import ValidationError
 
 
-players = [
+players_by_id = {
+    "95024a36": {"name": "Alec"},
+    "83ea724e": {"name": "Tay"}
+}
+
+all_players = [
     {"id": "95024a36", "name": "Alec"},
     {"id": "83ea724e", "name": "Tay"}
 ]
 
 @http()
 def main(req: HttpRequest):
-    logging.info(req)
-    logging.info(req.url)
-    logging.info(req.params)
+    logging.info(f"{req=}")
+    logging.info(f"{req.url=}")
+    logging.info(f"{req.params=}")
+    logging.info(f"{req.route_params=}")
     
-    return json.dumps(players)
+    if "player_id" in req.route_params:
+        player_id = req.route_params["player_id"]
+        if player_id not in players_by_id:
+            raise ValidationError(f"Player with id {player_id} not found!")
+        return json.dumps(players_by_id[player_id])
+    
+    return json.dumps(all_players)
 
 
 # -----------------------------------------------------------------------------
